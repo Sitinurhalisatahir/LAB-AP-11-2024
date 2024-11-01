fa8ae35707b11db2107e905e80c9674088c9adc1
fahira4 <nurulfahirah4@gmail.com>
2024-11-01 16:55:38 +0800

    DELETE

--- H071241038/No2.py
+++
@@ -1,9 +0,0 @@
-def kalimat_akronim(kalimat):
-    kata_akronim = kalimat.split()
-
-    akronim = ''.join([kata[0].upper() for kata in kata_akronim])
-    return akronim
-
-kalimat = input("masukkan kalimat: ")
-akronim = kalimat_akronim(kalimat)
-print(f"acronym {akronim}")
